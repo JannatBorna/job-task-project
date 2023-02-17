c4bcb4cccf79a653be1a3f09af0c8b756975ede5
Dilruba Jannat Borna <bornadilruba38@gmail.com>
2023-02-17 19:53:33 +0600

    first commit

--- src/component/page/Home.js
+++ src/component/page/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Row, Col, Nav } from 'react-bootstrap';
+import { Row, Col, Nav, Table } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {  faGauge, faClipboard, faUser, faBookOpenReader, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import {  faGauge, faClipboard, faUser, faBookOpenReader, faArrowRight, faSquareCheck } from "@fortawesome/free-solid-svg-icons";
 import Accordion from 'react-bootstrap/Accordion';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import girl1 from '../Image/girl-1.jpg';
@@ -141,53 +141,55 @@ const Home = () => {
                     </div>
            </div> 
 
-           <div className=''>
+           <div className='mt-5'>
             <Row>
                 <Col lg={4} md={4} sm={9}>
-                     <h4 className='booking-border mt-5'>Last Booking</h4>
+                     
                     <div className='booking-border-2'>
-                        <div className='booking mt-3'>
+                        <h4 className='mt-2'>Last Booking</h4>
+                        <hr />
+                        <div className='booking mt-3 p-1'>
                            <img src={girl1} alt="" /> 
                            <div className='booking-text mx-3'>
                                 <p> <span className='fw-bold'>Jannat Borna</span> <br />
                                 <span>To improve the text 2023</span></p>
                            </div>
                             <div>
-                                <FontAwesomeIcon icon={faArrowRight} className='mx-2'/>
+                                <FontAwesomeIcon icon={faArrowRight} className='arrow-icon'/>
                             </div>
                         </div>
-                        <div className='booking mt-3'>
+                        <div className='booking mt-3 p-1'>
                             <img src={girl2} alt="" /> 
                             <div className='booking-text mx-3'>
                                <p> <span className='fw-bold'>Jannat Borna</span> <br />
                                <span>To improve the text 2023</span></p>
                             </div>
                             <div>
-                                <FontAwesomeIcon icon={faArrowRight} className='mx-2'/>
+                                <FontAwesomeIcon icon={faArrowRight} className='arrow-icon'/>
                             </div>
                         </div>
-                        <div className='booking mt-3'>
+                        <div className='booking mt-3 p-1'>
                             <img src={boy1} alt="" /> 
                             <div className='booking-text mx-3'>
                                 <p> <span className='fw-bold'>Jannat Borna</span> <br />
                                 <span>To improve the text 2023</span></p>
                             </div>
                             <div>
-                                <FontAwesomeIcon icon={faArrowRight} className='mx-2'/>
+                                <FontAwesomeIcon icon={faArrowRight} className='arrow-icon'/>
                             </div>
                         </div>
-                        <div className='booking mt-3'>
+                        <div className='booking mt-3 p-1'>
                             <img src={boy2} alt="" /> 
                             <div className='booking-text mx-3'>
                                 <p> <span className='fw-bold'>Jannat Borna</span> <br />
                                 <span>To improve the text 2023</span></p>
                             </div>
                             <div>
-                                <FontAwesomeIcon icon={faArrowRight} className='mx-2'/>
+                                <FontAwesomeIcon icon={faArrowRight} className='arrow-icon'/>
                             </div>
                         </div>
-                        <div className='text-end'>
-                            <a href="/">See More</a>
+                        <div className='text-end mx-2'>
+                            <a href="/" className='a-tag'>See More</a>
                         </div>
                     </div>
                 </Col>
@@ -199,7 +201,260 @@ const Home = () => {
                 </Col>
             </Row>
            </div>
-        
+
+
+        <div className='mt-5'>
+
+    <Table striped bordered hover size="sm">
+        <thead>
+        <tr>
+          <td colSpan={7} className='text-start'>
+            <span className='summary'>Booking Summary</span>
+            <br />
+           <small>Open the Pulse app on your mobile phone</small>
+          </td>
+        </tr>
+      </thead>
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>CLIENT NAME</th>
+          <th>COMPAMY ID</th>
+          <th>BOOKING TO</th>
+          <th>COST OF HOTEL</th>
+          <th>STSTUS</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+            <td>#001</td>
+            <td>NK MARKO</td>
+            <td>#CM012G67</td>
+            <td>Hotel Arcone</td>
+            <td>$150</td>
+            <td>
+                <button className='status-panding'>panding</button>
+            </td>
+        </tr>
+        <tr>
+            <td>#002</td>
+            <td>NK MARKO</td>
+            <td>#CM012G67</td>
+            <td>Hotel Arcone</td>
+            <td>$150</td>
+            <td>
+                <button className='status-cencel'>cancel</button>
+            </td>
+        </tr>
+
+        <tr>
+            <td>#003</td>
+            <td>NK MARKO</td>
+            <td>#CM012G67</td>
+            <td>Hotel Arcone</td>
+            <td>$150</td>
+            <td>
+                <button className='status-approved'>approved</button>
+            </td>
+        </tr>
+
+         <tr>
+            <td>#004</td>
+            <td>NK MARKO</td>
+            <td>#CM012G67</td>
+            <td>Hotel Arcone</td>
+            <td>$150</td>
+            <td>
+                <button className='status-running'>running</button>
+            </td>
+        </tr>
+
+        <tr>
+            <td>#005</td>
+            <td>NK MARKO</td>
+            <td>#CM012G67</td>
+            <td>Hotel Arcone</td>
+            <td>$150</td>
+            <td>
+                <button className='status-panding'>panding</button>
+            </td>
+        </tr>
+
+        <tr>
+            <td>#006</td>
+            <td>NK MARKO</td>
+            <td>#CM012G67</td>
+            <td>Hotel Arcone</td>
+            <td>$150</td>
+            <td>
+                <button className='status-flowing'>flowing</button>
+            </td>
+        </tr>
+
+        <tr>
+            <td>#007</td>
+            <td>NK MARKO</td>
+            <td>#CM012G67</td>
+            <td>Hotel Arcone</td>
+            <td>$150</td>
+            <td>
+                <button className='status-process'>process</button>
+            </td>
+        </tr>
+
+        <tr>
+            <td colSpan={7} className='last-gap'></td>
+        </tr>
+      </tbody>
+    </Table>
+    </div>
+       
+
+                    <div className='last-eection'>
+                        <div>
+                            <div className='booking-border-2'>
+                            <h4 className='mt-2 mx-2 text-start text-h4'>Top Company conversion rate</h4>
+                            <p className='mx-2 text-start text-p'>Based on their performance</p>
+                        <hr />
+                        <div className='booking mt-3 p-1'>
+                           <img src={girl1} alt="" /> 
+                           <div className='booking-text mx-3'>
+                                <p> <span className='fw-bold'>Jannat Borna</span> <br />
+                                <span>To improve text 2023</span></p>
+                           </div>
+                            <div className='last-section-icon'>
+                                <h6>90%</h6>
+                            </div>
+                        </div>
+                        <div className='booking mt-3 p-1'>
+                            <img src={girl2} alt="" /> 
+                            <div className='booking-text mx-3'>
+                               <p> <span className='fw-bold'>Jannat Borna</span> <br />
+                               <span>To improve text 2023</span></p>
+                            </div>
+                            <div className='last-section-icon'>
+                                <h6>92%</h6>
+                            </div>
+                        </div>
+                        <div className='booking mt-3 p-1'>
+                            <img src={boy1} alt="" /> 
+                            <div className='booking-text mx-3'>
+                                <p> <span className='fw-bold'>Jannat Borna</span> <br />
+                                <span>To improve text 2023</span></p>
+                            </div>
+                            <div className='last-section-icon'>
+                                <h6>72%</h6>
+                            </div>
+                        </div>
+                        <div className='booking mt-3 p-1'>
+                            <img src={boy2} alt="" /> 
+                            <div className='booking-text mx-3'>
+                                <p> <span className='fw-bold'>Jannat Borna</span> <br />
+                                <span>To improve text 2023</span></p>
+                            </div>
+                            <div className='last-section-icon'>
+                                <h6>90%</h6>
+                            </div>
+                        </div>
+                        <div className='text-end mx-2'>
+                            <a href="/" className='a-tag'>See More</a>
+                        </div>
+                    </div>
+        </div>
+
+        <div>
+                        <div className='booking-border-2'>
+                            <h4 className='mt-2 mx-2 text-start text-h4'>Activity Account</h4>
+                            <p className='mx-2 text-start text-p'>Activity their account</p>
+                        <hr />
+                        <div className='booking mt-3 p-1'>
+                           <img src={girl1} alt="" /> 
+                           <div className='booking-text mx-3'>
+                                <p> <span className='fw-bold'>Jannat Borna</span> <br />
+                                <span>To improve the text 2023</span></p>
+                           </div>
+                            <div className='check-icon'>
+                                <FontAwesomeIcon icon={faSquareCheck} />
+                            </div>
+                        </div>
+                        <div className='booking mt-3 p-1'>
+                            <img src={girl2} alt="" /> 
+                            <div className='booking-text mx-3'>
+                               <p> <span className='fw-bold'>Jannat Borna</span> <br />
+                               <span>To improve the text 2023</span></p>
+                            </div>
+                            <div className='arrow-icon'>
+                                <FontAwesomeIcon icon={faSquareCheck} />
+                            </div>
+                        </div>
+                        <div className='booking mt-3 p-1'>
+                            <img src={boy1} alt="" /> 
+                            <div className='booking-text mx-3'>
+                                <p> <span className='fw-bold'>Jannat Borna</span> <br />
+                                <span>To improve the text 2023</span></p>
+                            </div>
+                            <div className='arrow-icon'>
+                                <FontAwesomeIcon icon={faSquareCheck} />
+                            </div>
+                        </div>
+                        <div className='booking mt-3 p-1'>
+                            <img src={boy2} alt="" /> 
+                            <div className='booking-text mx-3'>
+                                <p> <span className='fw-bold'>Jannat Borna</span> <br />
+                                <span>To improve the text 2023</span></p>
+                            </div>
+                            <div className='check-icon'>
+                                <FontAwesomeIcon icon={faSquareCheck} />
+                            </div>
+                        </div>
+                        <div className='text-end mx-2'>
+                            <a href="/" className='a-tag'>See More</a>
+                        </div>
+                    </div>
+                </div>
+
+          <div>                    
+            <div className='booking-border-2'>
+                        <h4 className='mx-2 text-start text-h4'>Monthly Income</h4>
+                        <p className='mx-2 text-start text-p'>income of last month and date </p>
+                        <hr />
+                        <div className='text-start p-1 bar'>
+                            <h5>$35000+</h5>
+                            <ProgressBar variant="danger" now={70} className='progress-icon'/>
+                            <p>Last month</p>
+                        </div>
+                        
+                        <div className='text-start p-1 bar'>
+                            <h5>$35000+</h5>
+                            <ProgressBar variant="info" now={80} className='progress-icon'/>
+                            <p>Last month</p>
+                        </div>
+
+                        <div className='text-start p-1 bar'>
+                            <h5>$35000+</h5>
+                            <ProgressBar variant="primany" now={60} className='progress-icon'/>
+                            <p>Last month</p>
+                        </div>
+                        {/* <div className='text-start p-1 bar'> */}
+                            {/* <h5>$35000+</h5> */}
+                            {/* <div className='progress-icon'> */}
+                                {/* <ProgressBar variant="success" now={75} className='progress-icon'/> */}
+                            {/* </div> */}
+                            {/* <p>Last month</p> */}
+                        {/* </div> */}
+                        <div className='text-end mx-2'>
+                            <a href="/" className='a-tag'>See More</a>
+                        </div>
+                    </div>
+        </div>
+
+
+
+
+
+
+     
+    </div>        
         </Col>
     </Row>
 
